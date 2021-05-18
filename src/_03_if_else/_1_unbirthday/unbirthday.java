20cb406ff29502263b59ba5f99f9348da346bb34
student <student@ip-172-31-61-176.us-west-2.compute.internal>
2021-05-18 15:27:26 -0700

    j

--- src/_03_if_else/_1_unbirthday/unbirthday.java
+++ src/_03_if_else/_1_unbirthday/unbirthday.java
@@ -5,7 +5,7 @@ import javax.swing.JOptionPane;
 public class unbirthday {
 	public static void main(String[] args) {
 	String birthday= JOptionPane.showInputDialog("what's your birthday?");
-		if (birthday.equals("3-9-2021")) {
+		if (birthday.equals("3-8-2021")) {
 			JOptionPane.showMessageDialog(null, "!HAPPY BIRTHDAY!!");
 		}
 		else {
